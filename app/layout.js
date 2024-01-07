e8b36a833e5c3a18e804b4b7c4d6db3e82345805
Yannic Kilcher <yk@users.noreply.github.com>
2024-01-07 18:40:51 +0100

    fix

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,4 @@
-// import Providers from "./providers";
+import Providers from "./providers";
 import { Inter } from "next/font/google";
 
 import "./globals.css";
@@ -14,8 +14,7 @@ export default function RootLayout({ children, params: { session } }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* <Providers session={session}>{children}</Providers> */}
-        {children}
+        <Providers session={session}>{children}</Providers>
       </body>
     </html>
   );
